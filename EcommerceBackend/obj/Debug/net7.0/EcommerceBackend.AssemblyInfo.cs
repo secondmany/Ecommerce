bdcf109d7b14c99e03963bdbd9be095001eee0c5
Trung Giang <giangthanhtrung@gmail.com>
2025-04-15 21:35:12 +0700

    Them xong Login API

--- EcommerceBackend/obj/Debug/net7.0/EcommerceBackend.AssemblyInfo.cs
+++ EcommerceBackend/obj/Debug/net7.0/EcommerceBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49deb4a8cca8a11cf53d55bf6d92ca3d12e9f5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
